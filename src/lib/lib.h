74fb1d84ffb1b1df0d655ec6acc39d549454b275
igush <igush@DESKTOP-3OTTB5S>
2022-04-18 13:38:39 +0300

    integral

--- src/lib/lib.h
+++ src/lib/lib.h
@@ -1,4 +1,17 @@
 #pragma once
 #include <cmath>
+#include <functional>
+
+using function = double(double);
+
+double Riman_integral(function func, double x0, double x1, double h) {
+  double res = 0;
+
+  for (double x = x0; x < x1; x+=h ) {
+    res += func(x + h / 2);
+  }
+
+  return res * h;
+}
 
 int foo() { return 10; }
--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,9 +1,11 @@
 #include "lib.h"
 #include <iostream>
+#include <cmath>
 
 
 int main()
 {
-   std::cout << foo()  << "\n";
-   return 0;
+  auto la = [](double x) { return sin(x); };
+  std::cout << Riman_integral(sin, 0, 1, 1e-2) << "\n";
+  return 0;
 }
