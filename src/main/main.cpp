9e26d4514b6498d723f321c6b1b664a47d54d8cd
igush <igush@DESKTOP-3OTTB5S>
2022-05-30 13:29:30 +0300

    Added graph creation

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -3,17 +3,22 @@
 #include <cmath>
 #include <numeric>
 #include <algorithm>
+#include <string>
+#include "matplotlibcpp.h"
 
 
 
 
+namespace plt = matplotlibcpp;
 
-
-int main()
+int main(int argc, char** argv)
 {
-  const int size = 1001;
+  int size = 1001;
+  if(argc == 2)
+    size = std::stoi(argv[1]);
   std::vector<double> vec(size, 0.0);
   std::iota(vec.begin(), vec.end(), 0);
+  std::vector<double> number_vec = vec;
   std::for_each(vec.begin(), vec.end(), [&](double& x) {x /= (size - 1); });
   SimpleBasis basis(std::move(vec));
 
@@ -27,9 +32,15 @@ int main()
 
   std::vector<double> res = calc.find_sol();
 
-  for (double v: res) {
-    std::cout << v << std::endl;
-  }
+  //for (double v: res) {
+  //  std::cout << v << std::endl;
+  //} 
 
+  plt::plot(number_vec, res);
+  plt::title("C array");
+  plt::xlabel("i");
+  plt::ylabel("Value of Ci");
+  plt::legend();
+  plt::show();
   return 0;
 }
